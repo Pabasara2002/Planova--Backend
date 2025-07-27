13559ef2f1baba9ecdd03af4c8b5b54d9682c65f
Pabasara Weerasooriya <pdewmini2002@gmail.com>
2025-07-27 17:48:47 +0530

    Added 7 model files to backend

---
+++ User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  // 2FA
+  twoFAEnabled: {
+    type: Boolean,
+    default: false
+  },
+  twoFASecret: String
+});
+
+module.exports = mongoose.model('User', userSchema);
+
